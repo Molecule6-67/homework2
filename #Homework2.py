c312bc2dfe3854d74f32dd0223165ace3818e74e
Aydar Sadykov <6darch6@vk.com>
2024-07-07 16:40:35 +0500

    Add files via upload

---
+++ #Homework2.py
@@ -0,0 +1,6 @@
+
+homework_count = 12
+Hours_spent = 1.5
+Course_name = 'Python'
+time_for_one_task = print((90/12)/60)
+print('Python ''homework_count:12 ''Hours_spent:12 ''time_for_one_task:0.125')
